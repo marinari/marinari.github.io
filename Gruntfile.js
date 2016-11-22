b06a4b9630d62f0e3b7793d7fafd6a9f3d8b8017
Arianna Marin <marin.arianna@gmail.com>
2016-11-21 21:20:21 -0500

    configured gruntfile.js to buildcontrol master branch

--- Gruntfile.js
+++ Gruntfile.js
@@ -41,7 +41,7 @@ module.exports = function (grunt) {
       pages: {
         options: {
           remote: 'git@github.com:marinari/marinari.github.io.git',
-          branch: 'develop'
+          branch: 'master'
         }
       }
     },
